8d527f40bc242f2ad8bf66208ab572112416699f
Vlad_Deineko <vladd1904@gmail.com>
2025-03-23 11:16:06 +0200

    task 3 done

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector("#name-input");
+
+const output = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+    if (event.currentTarget.value == 0) {
+        output.textContent = "Anonymous"
+    } else {
+        output.textContent = event.currentTarget.value;
+    };
+  });
