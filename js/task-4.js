1f587c4b5c90a2541ee8216b0385b51d5b62d6f9
Vlad_Deineko <vladd1904@gmail.com>
2025-03-23 11:30:07 +0200

    task 4 done

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,27 @@
+const loginForm = document.querySelector(".login-form")
 
+
+loginForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const { email, password } = loginForm.elements; 
+
+    // шоб данні користувача читались зручніше
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+
+
+      if (!emailValue || !passwordValue) {
+        alert("All form fields must be filled in");
+        return; 
+    }
+
+    const formData = {
+        email: emailValue,
+        password: passwordValue
+    };
+
+    console.log(formData);
+
+    loginForm.reset();
+});
