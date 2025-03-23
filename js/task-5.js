de56333df6874eebda8fe0b91af1c81a324d2237
Vlad_Deineko <vladd1904@gmail.com>
2025-03-23 12:55:50 +0200

    js final update

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+const bodyColor = document.querySelector("body");
+const widget = document.querySelector(".widget");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+widget.addEventListener("click", function (event) {
+  if (event.target.classList.contains("change-color")) {
+    const newColor = getRandomHexColor();  
+    bodyColor.style.backgroundColor = newColor;  
+    document.querySelector(".color").textContent = newColor; 
+  }
+});
